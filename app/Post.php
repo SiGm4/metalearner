d3c4ed73a6de857f272c09be00cdf768d7ea3c0f
SiGm4 <dimgmx@hotmail.com>
2017-11-07 22:45:52 +0200

    Many to many tables and model updates for categs and tags

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,18 @@ class Post extends Model
         return $this->belongsTo('App\User','user_id','id');
         
     }
+
+    public function categories(){
+        
+        return $this->belongsToMany('App\Category','post_category','post_id','category_id');
+        
+    }
+
+    public function tags(){
+        
+        return $this->belongsToMany('App\Tag','post_tag','post_id','tag_id');
+        
+    }
+
+ 
 }
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -11,5 +11,26 @@
             <li class="{{ Request::is('contact') ? 'active' : '' }}"><a href="/contact">Contact</a></li>
             <li class="{{ Request::is('posts') ? 'active' : '' }}"><a href="/posts">Posts</a></li>
         </ul>
+
+        @auth
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hello {{ Auth::user()->email }} <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/posts">Posts</a></li>
+                    <li><a href="/categories">Categories</a></li>
+                    <li><a href="/tags">Tags</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li>
+                        <form method="POST" action="/logout"> 
+                            <input type="submit" value="Logout" class="btn btn-link" id="logoutBtn"> 
+                            {{ csrf_field() }}  
+                        </form> 
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        @endauth
+
     </div>
 </nav>
